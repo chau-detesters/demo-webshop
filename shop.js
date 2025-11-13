8a78ab6d5e101e4bac9d428762ec2a5b1d5a044d
gcnguyen <drgnsoul@gmail.com>
2025-11-13 14:21:10 +0900

    combine-line-items #6

--- shop.js
+++ shop.js
@@ -37,21 +37,24 @@ function renderBasket() {
     if (cartButtonsRow) cartButtonsRow.style.display = "none";
     return;
   }
-  // Count occurrences of each product
+  
+  // Group identical products and count quantities
   const productCounts = {};
   basket.forEach((product) => {
-    productCounts[product] = (productCounts[product] || 0) + 1;
-  });
-  // Display each unique product with its quantity
-  Object.keys(productCounts).forEach((productId) => {
-    const item = PRODUCTS[productId];
-    if (item) {
-      const quantity = productCounts[productId];
-      const li = document.createElement("li");
-      li.innerHTML = `<span class='basket-emoji'>${item.emoji}</span> <span>${quantity}x ${item.name}</span>`;
-      basketList.appendChild(li);
+    if (PRODUCTS[product]) {
+      productCounts[product] = (productCounts[product] || 0) + 1;
     }
   });
+  
+  // Display grouped products with quantities
+  Object.keys(productCounts).forEach((product) => {
+    const item = PRODUCTS[product];
+    const quantity = productCounts[product];
+    const li = document.createElement("li");
+    li.innerHTML = `<span class='basket-emoji'>${item.emoji}</span> <span>${quantity}x ${item.name}</span>`;
+    basketList.appendChild(li);
+  });
+  
   if (cartButtonsRow) cartButtonsRow.style.display = "flex";
 }
 
